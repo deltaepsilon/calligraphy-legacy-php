1d08562bc5a62d7589398ef929dc5f688da202d5
Chris Esplin <christopher.esplin@gmail.com>
2013-08-14 16:33:28 -0600

    Getting rid of a bad extends error.

--- src/CDE/UserBundle/Entity/User.php
+++ src/CDE/UserBundle/Entity/User.php
@@ -2,7 +2,7 @@
 namespace CDE\UserBundle\Entity;
 
 use CDE\AffiliateBundle\Entity\Affiliate;
-use FOS\UserBundle\Entity\User as BaseUser;
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
 
