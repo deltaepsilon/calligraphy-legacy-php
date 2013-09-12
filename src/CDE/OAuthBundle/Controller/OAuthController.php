d40150e1076c2ccd8885434e523ad0745e6c3e6a
Chris Esplin <christopher.esplin@gmail.com>
2013-09-12 14:48:47 -0600

    OAuth tokens are being issued and can be revoked and refreshed. Do not know if they can actually be used against the app yet.

--- src/CDE/OAuthBundle/Controller/OAuthController.php
+++ src/CDE/OAuthBundle/Controller/OAuthController.php
@@ -9,10 +9,10 @@ class OAuthController extends Controller
     protected  function getClientManager() {
         return $this->get('cde_oauth.manager.client');
     }
-    public function indexAction($page = 1)
+    public function clientsAction($page = 1)
     {
         if (isset($_GET['code'])) {
-            return $this->redirect($this->generateUrl('CDEOAuthBundle_oauth_token', array(
+            return $this->redirect($this->generateUrl('CDEOAuthBundle_client_token', array(
                 'code' => $_GET['code'],
             )));
         }
@@ -23,13 +23,13 @@ class OAuthController extends Controller
         ));
     }
 
-    public function createAction()
+    public function clientCreateAction()
     {
-        $redirectURI = $this->generateUrl('CDEOAuthBundle_oauth_client');
+        $redirectURI = $this->generateUrl('CDEOAuthBundle_client_index');
 
         $client = $this->getClientManager()->create();
         $client->setRedirectUris(array($redirectURI));
-        $client->setAllowedGrantTypes(array('token', 'authorization_code'));
+        $client->setAllowedGrantTypes(array('token', 'authorization_code', 'refresh_token'));
         $this->getClientManager()->add($client);
 
         return $this->redirect($this->generateUrl('fos_oauth_server_authorize', array(
@@ -39,17 +39,17 @@ class OAuthController extends Controller
         )));
     }
 
-    public function deleteAction($id)
+    public function clientDeleteAction($id)
     {
         $client = $this->getClientManager()->find($id);
         $this->getClientManager()->remove($client);
         $this->get('session')->getFlashBag()->add('notice', "Deleted ".$id);
-        return $this->redirect($this->generateUrl('CDEOAuthBundle_oauth_client'));
+        return $this->redirect($this->generateUrl('CDEOAuthBundle_client_index'));
     }
 
     public function tokenAction() {
         $code = $_GET['code'];
-        $authCode = $this->getClientManager()->findByCode($code);
+        $authCode = $this->getClientManager()->findByAuthCode($code);
         $client = $authCode->getClient();
         if (isset($_SERVER['HTTP_X_FORWARDED_PROTO'])) {
             $proto = $_SERVER['HTTP_X_FORWARDED_PROTO'];
@@ -61,9 +61,42 @@ class OAuthController extends Controller
             'client_secret' => $client->getSecret(),
             'grant_type' => 'authorization_code',
             'code' => $code,
-            'redirect_uri' => $this->generateUrl('CDEOAuthBundle_oauth_client'),
+            'redirect_uri' => $this->generateUrl('CDEOAuthBundle_client_index'),
         )));
         $token = json_decode($json);
-        return $this->redirect($this->generateUrl('CDEOAuthBundle_oauth_client', (array) $token));
+        return $this->redirect($this->generateUrl('CDEOAuthBundle_client_index', (array) $token));
     }
+
+    public function refreshAction($token) {
+        $refreshToken = $this->getClientManager()->findByRefreshToken($token);
+        $client = $refreshToken->getClient();
+        if (isset($_SERVER['HTTP_X_FORWARDED_PROTO'])) {
+            $proto = $_SERVER['HTTP_X_FORWARDED_PROTO'];
+        } else {
+            $proto = 'http';
+        }
+        $accessTokens = $client->getAccessToken();
+        foreach ($accessTokens as $accessToken) {
+            $this->getClientManager()->removeAccessToken($accessToken);
+        }
+
+        $json = file_get_contents($proto.'://'.$_SERVER['HTTP_HOST'].$this->generateUrl('fos_oauth_server_token', array(
+            'client_id' => $client->getId().'_'.$client->getRandomId(),
+            'client_secret' => $client->getSecret(),
+            'grant_type' => 'refresh_token',
+            'refresh_token' => $token,
+            'redirect_uri' => $this->generateUrl('CDEOAuthBundle_client_index'),
+        )));
+        $token = json_decode($json);
+        $this->get('session')->getFlashBag()->add('notice', "Refreshed ".$client->getId());
+        return $this->redirect($this->generateUrl('CDEOAuthBundle_client_index', (array) $token));
+    }
+
+    public function forwardAction() {
+        $url = parse_url($_GET['redirect']);
+        $redirect = $url['scheme'].'://'.$url['host'];
+        $token = $this->getClientManager()->getToken($this->getUser());
+        return $this->redirect($redirect.'?access_token='.$token->getToken());
+    }
+
 }
--- src/CDE/OAuthBundle/Entity/ClientManager.php
+++ src/CDE/OAuthBundle/Entity/ClientManager.php
@@ -10,6 +10,7 @@
 namespace CDE\OAuthBundle\Entity;
 
 
+use CDE\UserBundle\Entity\User;
 use Doctrine\ORM\EntityManager;
 
 class ClientManager {
@@ -18,10 +19,12 @@ class ClientManager {
     protected $clientManager;
     protected $paginator;
 
-    public function __construct(EntityManager $em, $class, $codeClass, $clientManager, $paginator) {
+    public function __construct(EntityManager $em, $class, $codeClass, $accessClass, $refreshClass, $clientManager, $paginator) {
         $this->em = $em;
         $this->repo = $this->em->getRepository($class);
         $this->codeRepo = $this->em->getRepository($codeClass);
+        $this->accessRepo = $this->em->getRepository($accessClass);
+        $this->refreshRepo = $this->em->getRepository($refreshClass);
         $this->class = $class;
         $this->clientManager = $clientManager;
         $this->paginator = $paginator;
@@ -44,12 +47,25 @@ class ClientManager {
         $this->em->flush();
     }
 
+    public function removeAccessToken(AccessToken $token) {
+        $this->em->remove($token);
+        $this->em->flush();
+    }
+
     public function find($id) {
         return $client = $this->repo->find($id);
     }
 
-    public function findByCode($code) {
-        return $client = $this->codeRepo->findOneByToken($code);
+    public function findByAuthCode($token) {
+        return $client = $this->codeRepo->findOneByToken($token);
+    }
+
+    public function findByAuthCodeId($id) {
+        return $client = $this->codeRepo->find($id);
+    }
+
+    public function findByRefreshToken($token) {
+        return $client = $this->refreshRepo->findOneByToken($token);
     }
 
     public function findByPage($page = 1, $limit = 10) {
@@ -66,6 +82,10 @@ class ClientManager {
         );
 
         return $pagination;
+    }
+
+    public function getToken(User $user) {
+        return $token = $this->accessRepo->findOneByUser($user);
     }
 
 }
