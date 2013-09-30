770b1209664b36b4a0d467f9f061961f071be82f
Chris Esplin <christopher.esplin@gmail.com>
2013-09-30 15:20:08 -0600

    Adding angular/images route to grab image listings from AWS.

--- src/CDE/UtilityBundle/Controller/AngularController.php
+++ src/CDE/UtilityBundle/Controller/AngularController.php
@@ -3,15 +3,28 @@
 namespace CDE\UtilityBundle\Controller;
 
 use CDE\UserBundle\Controller\UserController;
+use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class AngularController extends Controller
+class AngularController extends FOSRestController
 {
+    protected function getAWSService() {
+        return $this->get('cde_utility.manager.aws');
+    }
     public function loginAction()
     {
         $userController = new UserController();
         $userController->container = $this->container;
         return $userController->loginPartialAction();
+    }
+
+    public function listImagesAction($prefix)
+    {
+        $response = $this->getAWSService()->listImages($prefix);
+        $view = $this->view($response, 200)
+            ->setHeader('Expires', gmdate('D, d M Y H:i:s', strtotime('+1 days')) . ' GMT')
+            ->setFormat('json');
+        return $this->handleView($view);
     }
 
 }
--- src/CDE/UtilityBundle/Tests/Controller/AngularControllerTest.php
+++ src/CDE/UtilityBundle/Tests/Controller/AngularControllerTest.php
@@ -6,11 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class AngularControllerTest extends WebTestCase
 {
-    public function testLogin()
+//    public function testLogin()
+//    {
+//        $client = static::createClient();
+//
+//        $crawler = $client->request('GET', '/login');
+//    }
+
+    public function testListBucket()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/login');
+        $crawler = $client->request('GET', '/angular/images/calligraphy|assets|melissa');
+        $response = json_decode($client->getResponse()->getContent());
+        $this->assertEquals($client->getResponse()->getStatusCode(), 200);
+        $this->assertTrue(is_array($response));
     }
 
 }
